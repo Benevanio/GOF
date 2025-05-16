af7beb5cdba6efbefeff5c7f66b2239b9fab25a8
Benevanio <benevaniosantos930@gmail.com>
2025-05-15 21:18:51 -0300

    feat: add user display and removal functionality in index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,10 @@ console.log(db1 === db2); // true
 db1.addUser({ name: "John", age: 30 });
 const users = db2.getUsers();
 console.log(users);
+db1.getUsers().forEach(user => {
+    console.log(`Name: ${user.name}, Age: ${user.age}`);
+})
+db1.removeUser({ name: "John", age: 30 });
+db1.getUsers().forEach(user => {        
+    console.log(`Name: ${user.name}, Age: ${user.age}`);
+})

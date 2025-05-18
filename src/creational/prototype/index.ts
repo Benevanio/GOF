957ec164edaa1a0a93f77e821bc8763cca086046
Benevanio <benevaniosantos930@gmail.com>
2025-05-18 09:20:05 -0300

    feat: add address to person1 instance in prototype example

--- src/creational/prototype/index.ts
+++ src/creational/prototype/index.ts
@@ -1,6 +1,8 @@
+import { Address } from "./proto/address.js";
 import { Person } from "./proto/prototype.js";
 
 const person1 = new Person("John Doe", 30, "123 Main St");
+person1.addresses.push(new Address("456 Elm St", "Los Angeles", "USA"));
 const person2 = person1.clone();
 
 console.log(person1);

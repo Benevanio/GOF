e2fd27f5f161aeeba50890cbab153f82a12ab52c
Benevanio <benevaniosantos930@gmail.com>
2025-05-20 19:48:42 -0300

    feat: update vehicle creation to use specific customer names

--- src/creational/abstract-factory/index.ts
+++ src/creational/abstract-factory/index.ts
@@ -1,12 +1,12 @@
 import { EnterpriseCreateVehicleCustomerFactory } from "./factory/enterprice-vehicle-factory.js";
 
 const enterprise = new EnterpriseCreateVehicleCustomerFactory();
-const vehicle = enterprise.createVehicle("Car", "Enterprise");
+const vehicle = enterprise.createVehicle("Car", "paula");
 vehicle.pickup();
-const vehicle2 = enterprise.createVehicle("Truck", "Enterprise");
+const vehicle2 = enterprise.createVehicle("Truck", "Joana");
 vehicle2.pickup();
-const vehicle3 = enterprise.createVehicle("Bus", "Enterprise");
+const vehicle3 = enterprise.createVehicle("Bus", "Matheus");
 vehicle3.pickup();
 
-const vehicle4 = enterprise.createVehicle("Van", "Enterprise");
+const vehicle4 = enterprise.createVehicle("Van", "Lucas");
 vehicle4.pickup();

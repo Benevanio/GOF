bd9c07f3981f207f3f018b5073cf3dce6a170ac6
Benevanio <benevaniosantos930@gmail.com>
2025-05-20 19:44:32 -0300

    feat: implement vehicle creation functionality in abstract factory pattern

--- src/creational/abstract-factory/index.ts
+++ src/creational/abstract-factory/index.ts
@@ -1,1 +1,12 @@
-console.log("Abstract Factory Pattern");
+import { EnterpriseCreateVehicleCustomerFactory } from "./factory/enterprice-vehicle-factory.js";
+
+const enterprise = new EnterpriseCreateVehicleCustomerFactory();
+const vehicle = enterprise.createVehicle("Car", "Enterprise");
+vehicle.pickup();
+const vehicle2 = enterprise.createVehicle("Truck", "Enterprise");
+vehicle2.pickup();
+const vehicle3 = enterprise.createVehicle("Bus", "Enterprise");
+vehicle3.pickup();
+
+const vehicle4 = enterprise.createVehicle("Van", "Enterprise");
+vehicle4.pickup();

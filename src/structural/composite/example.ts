75ad51bebdc9c571b9a217ce3dfddba17126bb1d
Benevanio <benevaniosantos930@gmail.com>
2025-05-21 20:41:04 -0300

    feat: refactor example to use ProductComposite for total price calculation

--- src/structural/composite/example.ts
+++ src/structural/composite/example.ts
@@ -1,8 +1,13 @@
 import { ProductLeaf } from "./class/product-Leaf.js";
+import { ProductComposite } from "./class/ProductComposed.js";
 
 const camiseta = new ProductLeaf("Camiseta", 10);
 const calca = new ProductLeaf("Calça", 20);
 const sapato = new ProductLeaf("Sapato", 30);
-const produtos = [camiseta, calca, sapato];
-const total = produtos.reduce((total, produto) => total + produto.getPrice(), 0);
-console.log(`Total: R$ ${total}`);
+const productBox = new ProductComposite("Caixa de produtos");
+
+productBox.add(camiseta);
+productBox.add(calca);
+productBox.add(sapato);
+const totalPrice = productBox.getPrice();
+console.log(`O preço total dos produtos na ${productBox.getName()} é: R$ ${totalPrice}`);

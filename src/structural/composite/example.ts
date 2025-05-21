29c4c5b6b60e33ecd7d225a05ba6964f010e65d8
Benevanio <benevaniosantos930@gmail.com>
2025-05-21 20:46:41 -0300

    feat: extend example to include additional ProductLeaf and ProductComposite instances with total price calculation

--- src/structural/composite/example.ts
+++ src/structural/composite/example.ts
@@ -11,3 +11,15 @@ productBox.add(calca);
 productBox.add(sapato);
 const totalPrice = productBox.getPrice();
 console.log(`O preço total dos produtos na ${productBox.getName()} é: R$ ${totalPrice}`);
+
+const table = new ProductLeaf("Table", 4500);
+const kindle = new ProductLeaf("Kindle", 500);
+const book = new ProductLeaf("Book", 50);
+const productBox2 = new ProductComposite("Caixa de produtos 2");
+productBox2.add(table);
+productBox2.add(kindle);
+productBox2.add(book);
+productBox2.add(productBox);
+console.log("Teste " + productBox2.getPrice());
+const totalPrice2 = productBox2.getPrice();
+console.log(`O preço total dos produtos na ${productBox2.getName()} é: R$ ${totalPrice2}`);

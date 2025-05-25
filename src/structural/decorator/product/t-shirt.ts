d9e85b9ebd2e9918cc609cb1174f036367e52590
Benevanio <benevaniosantos930@gmail.com>
2025-05-25 11:43:54 -0300

    feat: add IProduct interface and TShirt class implementation

---
+++ src/structural/decorator/product/t-shirt.ts
@@ -0,0 +1,19 @@
+import { IProduct } from "./Iproductor";
+
+export class TShirt implements IProduct {
+    private name: string;
+    private price: number;
+
+    constructor(name: string, price: number) {
+        this.name = name;
+        this.price = price;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getPrice(): number {
+        return this.price;
+    }
+}

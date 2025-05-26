e1914b45946ae2f563be6faa9ad9b22a4b56ab8c
Benevanio <benevaniosantos930@gmail.com>
2025-05-26 19:41:24 -0300

    feat: refactor DeliveryFactory to use makeLocation method for DeliveryLocation creation

--- src/structural/flyweight/delivery/delivery-factory.ts
+++ src/structural/flyweight/delivery/delivery-factory.ts
@@ -1,14 +1,20 @@
 import { DeliveryLocationData } from "../types/delivery-types";
 import { DeliveryLocation } from "./delivery-location";
+import { IDeliveryFlyweight } from "./Idelivery-flyweight";
 
 export class DeliveryFactory {
     private readonly locations: Map<string, DeliveryLocation> = new Map();
 
-    getDeliveryLocation(data: DeliveryLocationData): DeliveryLocation {
-        const key = data.locationId;
-        if (!this.locations.has(key)) {
-            this.locations.set(key, new DeliveryLocation(data));
+    makeLocation(
+        locationData: DeliveryLocationData): IDeliveryFlyweight {
+        const { locationId } = locationData;
+        if (!this.locations.has(locationId)) {
+            this.locations.set(
+                locationId,
+                new DeliveryLocation(locationData)
+            );
         }
-        return this.locations.get(key)!;
+        return this.locations.get(locationId) as IDeliveryFlyweight;
     }
+
 }
